f7e8dd36c0a2e8970218cb486a657be10c166c7d
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-07-25 16:45:56 -0400

    Updates

---
+++ precache-manifest.37daacaf1fc1eba7ee04e1af13debc47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fc9031d41c5889a6d6c00f43509e36",
+    "url": "/FenixTrax18/Employee-Directory/index.html"
+  },
+  {
+    "revision": "b7802236b305728c7316",
+    "url": "/FenixTrax18/Employee-Directory/static/css/main.b8f6238e.chunk.css"
+  },
+  {
+    "revision": "b925186bda2bd5f10a83",
+    "url": "/FenixTrax18/Employee-Directory/static/js/2.5497271e.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/FenixTrax18/Employee-Directory/static/js/2.5497271e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7802236b305728c7316",
+    "url": "/FenixTrax18/Employee-Directory/static/js/main.4227063d.chunk.js"
+  },
+  {
+    "revision": "7cd0d92996bba4f456f7",
+    "url": "/FenixTrax18/Employee-Directory/static/js/runtime-main.4f2fc3d3.js"
+  }
+]);
